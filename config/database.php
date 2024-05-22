54d3958ff027a3a6eff46fe0afabc53f50d248e6
Elias Craveiro <elias7.5@hotmail.com>
2024-05-22 08:18:33 -0300

    DB config

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 // Configuração do banco de dados
-define("DB_HOST", "localhost");
-define("DB_USER", "finance_controller");
-define("DB_PASSWORD", "D-p9lc^&d6/n=!%k3h");
-define("DB_NAME", "finance_controller");
+define("DB_HOST", "maonaroda.postgresql.dbaas.com.br");
+define("DB_USER", "maonaroda");
+define("DB_PASSWORD", "Dc7yM9yajVa5S@");
+define("DB_NAME", "maonaroda");
