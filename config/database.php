574d5d073a01bde5cf253f14cbff7a2e63938d28
eliassr75 <50608433+eliassr75@users.noreply.github.com>
2024-06-03 11:23:34 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,3 +6,8 @@ define("DB_HOST", $parsed['DB_HOST']);
 define("DB_USER", $parsed['DB_USER']);
 define("DB_PASSWORD", $parsed['DB_PASSWORD']);
 define("DB_NAME", $parsed['DB_NAME']);
+
+define("MAIL_HOST", $parsed['MAIL_HOST']);
+define("MAIL_USERNAME", $parsed['MAIL_USERNAME']);
+define("MAIL_PASSWORD", $parsed['MAIL_PASSWORD']);
+define("MAIL_PORT", $parsed['MAIL_PORT']);
